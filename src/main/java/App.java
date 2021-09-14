bff53c237a9936ae28ef2f0ea6c968f7598a32a4
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-09-14 23:29:29 +0600

    Added hook

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,26 @@
 import gui.Display;
 import gui.util._Win;
+import keyListaner.Capture;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
 
 public class App {
     public static void main(String[] args) {
         long start=System.currentTimeMillis();
         new _Win();
         new Display();
+
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
         System.out.println(System.currentTimeMillis()-start);
+        GlobalScreen.addNativeKeyListener(new Capture());
+
     }
 }
---
+++ src/main/java/keyListaner/Capture.java
@@ -0,0 +1,29 @@
+package keyListaner;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class Capture implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+
+}
